7c7988aa61eee0bcac6e678dc8a7802f4e2ac659
rafael moura <rafaelmoura2929@gmail.com>
2020-05-06 14:30:10 -0300

    Adicionando Redu no contador

--- contador/main.js
+++ contador/main.js
@@ -8,26 +8,13 @@ const counter = (state = 0, action) => {
     return state
 }
 
-console.assert(
-    counter(0, { type: 'INCREMENT' }) === 1
-)
+const { createStore } = Redux
+const store = createStore(counter)
 
-console.assert(
-    counter(1, { type: 'INCREMENT' }) === 2
-)
+store.subscribe(() => {
+    console.log("State:", store.getState())
+})
 
-console.assert(
-    counter(2, { type: 'DECREMENT' }) === 1
-)
-
-console.assert(
-    counter(5, { type: 'DECREMENT' }) === 4
-)
-
-console.assert(
-    counter(3, { type: 'SOMETHING' }) === 3
-)
-
-console.assert(
-    counter(undefined, {}) === 0
-)
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'DECREMENT' })
