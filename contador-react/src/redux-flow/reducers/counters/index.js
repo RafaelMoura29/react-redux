e834311de56c707277fa1c57d2c9de1f04b85fe3
rafael moura <rafaelmoura2929@gmail.com>
2020-05-15 15:18:46 -0300

    Adicionando testes de exceções e adicionando boas praticas no reducer

--- contador-react/src/redux-flow/reducers/counters/index.js
+++ contador-react/src/redux-flow/reducers/counters/index.js
@@ -1,14 +1,22 @@
 'use strict'
 
-export default (state, action) => {
+export const ADD_COUNTER = 'ADD_COUNTER'
+export const REMOVE_COUNTER = 'REMOVE_COUNTER'
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const initialState = []
+
+export default (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_COUNTER':
+    case ADD_COUNTER:
       return state.concat(0)
-    case 'REMOVE_COUNTER':
+    case REMOVE_COUNTER:
       return state.filter((_, index) => index !== action.index)
-    case 'INCREMENT':
+    case INCREMENT:
       return state.map((counter, index) => index === action.index ? counter + 1 : counter)
-    case 'DECREMENT':
+    case DECREMENT:
       return state.map((counter, index) => index === action.index ? counter - 1 : counter)
   }
+  return state
 }
